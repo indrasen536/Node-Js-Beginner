4c82eda58a9deb5db2455eaa0345d1620048acd2
indrasen536 <indrasen536@gmail.com>
2023-03-09 03:47:23 +0530

    JWT authentication for login user

--- backend-project/controllers/userController.js
+++ backend-project/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModels");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@route POST/api/users/reigster
@@ -40,8 +41,33 @@ const registerUser = asyncHandler(async (req, res) => {
 //@desc login a user
 //@route POST/api/users/login
 //@access public
-const loginUser = asyncHandler((req, res) => {
-  res.json({ message: "login user" });
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!!");
+  }
+
+  const user = await User.findOne({ email });
+  //compare password with hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Email or Password Incorrect");
+  }
+  //res.json({ message: "login user" });
 });
 
 //@desc Current user
